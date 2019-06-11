ecda9cdbbc89a9efa69eaf8e079140e16a16319c
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-10 19:08:07 -1000

    finished query methods

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 
 console.log(process.env.API_TOKEN);
@@ -11,7 +12,14 @@ app.use(morgan('dev'));
 const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  
   console.log('validate bearer token middleware');
+  
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request'});
+  }
   // move to the next middleware
   next();
 });
@@ -23,7 +31,35 @@ function handleGetTypes(req, res) {
 app.get("/types", handleGetTypes);
 
 function handleGetPokemon(req, res) {
-  res.send('Hello, Pokemon!');
+  const { type, name } = req.query;
+  let results = POKEDEX.pokemon;
+
+  if (!name && !type) {
+    return res.status(200).json(results);
+  }
+
+  if (name && type) {
+    return res.status(400).send('Query must be one of type or name');
+  }
+
+  if (!name) {
+    let lowerCaseArr = validTypes.map(type => type.toLowerCase()).includes(type.toLowerCase());
+    if (!lowerCaseArr) {
+      return res.status(400).send(`Type must be one of: ${validTypes.join(', ')}`);
+    }
+    results = results.filter(poke => poke.type.map(el => el.toLowerCase()).includes(type.toLowerCase()));
+  }
+
+  if(!type) {
+    const lowerCaseName = name.toLowerCase();
+    if (!results.filter(poke => poke.name.toLowerCase().includes(lowerCaseName))) {
+      return res.status(400).send('Pokemon name must exist');
+    };
+    results = results.filter(poke => poke.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+
+  return res.status(200).json(results);
 };
 
 app.get('/pokemon', handleGetPokemon);
